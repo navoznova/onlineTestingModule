161ba4e217d6ba599ff6215af9bdc73224cf2fec
Anna Navoznova <anna.navoznova@gmail.com>
2022-01-15 16:34:07 +0300

    feat: add new components

--- src/app/main/lessonCard/lessonCard.module.ts
+++ src/app/main/lessonCard/lessonCard.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AuthorComponent } from './author/author.component';
+import { DescriptionComponent } from './description/description.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CommentsComponent } from './comments/comments.component';
+
 
 
 @NgModule({
   declarations: [
     AuthorComponent,
+    DescriptionComponent,
+    StatisticsComponent,
+    CommentsComponent
   ],
   exports: [
-    AuthorComponent
+    AuthorComponent,
+    DescriptionComponent,
+    StatisticsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
