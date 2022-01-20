c692cec102e5d951f1b888b2796d9f0ca88933ec
Anna Navoznova <anna.navoznova@gmail.com>
2022-01-20 19:34:25 +0300

    fix: delete head component

--- src/app/main/lessonCard/lessonCard.module.ts
+++ src/app/main/lessonCard/lessonCard.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AuthorComponent } from './author/author.component';
-import { HeadComponent } from './head/head.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { CommentsComponent } from './comments/comments.component';
 import { StatisticsModule } from './statistics/statistics.module';
@@ -11,13 +10,11 @@ import { StatisticsModule } from './statistics/statistics.module';
 @NgModule({
   declarations: [
     AuthorComponent,
-    HeadComponent,
     StatisticsComponent,
-    CommentsComponent
+    CommentsComponent,
   ],
   exports: [
     AuthorComponent,
-    HeadComponent,
     StatisticsComponent,
     CommentsComponent,
   ],
