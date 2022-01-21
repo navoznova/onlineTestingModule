72247173e73cccfcd692b7b745544a7c482bd5f7
Anna Navoznova <anna.navoznova@gmail.com>
2022-01-21 22:10:01 +0300

    feat: add field for widget

--- src/app/main/lessonCard/lessonCard.component.ts
+++ src/app/main/lessonCard/lessonCard.component.ts
@@ -21,6 +21,12 @@ export class LessonCardComponent implements OnInit {
     tabsContents: string[] = ["No content", "два2 контент", "Три три контент",]
     currentTabIndex: number = 0;
 
+    comments: number = 1;
+    rating: number = 1;
+    shares: number = 1;
+    likes: number = 1;
+    views: number = 1;
+
     constructor(private http: HttpClient) { }
 
     ngOnInit(): void {
@@ -32,11 +38,18 @@ export class LessonCardComponent implements OnInit {
                 console.log(`sub_title = ${lesson.sub_title}`);
                 console.log(`first_name = ${lesson.author?.first_name}`);
                 console.log(`published_at = ${lesson.published_at}`);
+                console.log(`published_at = ${lesson.published_at}`);
 
                 this.lessonId = lesson.id || '';
                 this.lessonTitle = lesson.title || '';
                 this.lessonSubTitle = lesson.sub_title || '';
                 this.tabsContents[0] = lesson.description || '';
+
+                this.comments = lesson.stats?.comments_count || 1;
+                this.rating = lesson.stats?.rating || 1;
+                this.shares = lesson.stats?.shares_count || 1;
+                this.likes = lesson.stats?.likes_count || 1;
+                this.views = lesson.stats?.views_count || 1;
 
                 const authorFirstName = lesson.author?.first_name || '';
                 const authorLastName = lesson.author?.last_name || '';
@@ -48,9 +61,9 @@ export class LessonCardComponent implements OnInit {
             });
     }
 
-    setActiveTab(index: number){
-		this.currentTabIndex = index;
-	}
+    setActiveTab(index: number) {
+        this.currentTabIndex = index;
+    }
 }
 
 class LessonResponseModel {
@@ -60,6 +73,7 @@ class LessonResponseModel {
     description: string | undefined;
     author: AuthorResponseModel | undefined;
     published_at: Date | undefined;
+    stats: Stats | undefined;
 }
 
 class AuthorResponseModel {
@@ -74,5 +88,15 @@ class UserpicResponseModel {
 
 class SmResponseModel {
     url: string | undefined;
+}
+
+class Stats {
+    comments_count: number | undefined;
+    rating: number | undefined;
+    shares_count: number | undefined;
+    likes_count: number | undefined;
+    dislikes_count: number | undefined;
+    views_count: number | undefined;
+
 }
 
