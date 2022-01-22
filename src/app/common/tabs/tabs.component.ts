f86800301b791265867d388adc1df072f4f313c8
Anna Navoznova <anna.navoznova@gmail.com>
2022-01-22 20:30:29 +0300

    feat: add style for activ tab

--- src/app/common/tabs/tabs.component.ts
+++ src/app/common/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
 	selector: 'app-tabs',
@@ -13,11 +13,13 @@ export class TabsComponent {
 	public contents: string[] = [];
 
 	currentTabIndex: number = 0;
+	public activeItem: number | undefined;
 
 	constructor() { }
 
 	setActiveTab(index: number){
 		this.currentTabIndex = index;
+		this.activeItem = index;
 	}
 }
 
